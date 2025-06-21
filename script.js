2667ac00fe71a027560a6a138b4f1322f457f814
cod10129 <110200933+cod10129@users.noreply.github.com>
2025-06-20 22:52:13 -0500

    add phase counter and spare tracker

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var turnsElapsed = 0;
+var turnsOnCurrentPhase = 0;
 var battlePhase = 1;
 
 /** Representation of Jevil's statistics. */
@@ -10,12 +11,66 @@ var jevil = {
 
 function advanceTurn() {
     turnsElapsed += 1;
+    turnsOnCurrentPhase += 1;
 
     document.getElementById("turn-num").textContent = turnsElapsed;
+
+    const previousPhase = battlePhase;
+    recomputeBattlePhase();
+    if (previousPhase !== battlePhase) {
+        turnsOnCurrentPhase = 1;
+    }
     document.getElementById("phase-num").textContent = battlePhase;
+
+    updateSpareConditions();
+}
+
+// TODO - Implement some way of entering phase 5
+function recomputeBattlePhase() {
+    // Don't exit from Phase 5
+    if (battlePhase === 5) { return }
+
+    if (jevil.tiredness >= 6) {
+        battlePhase = 4;
+    } else if (jevil.tiredness >= 4) {
+        battlePhase = 3;
+    } else if (jevil.tiredness >= 2) {
+        battlePhase = 2;
+    } else {
+        battlePhase = 1;
+    }
+}
+
+function updateSpareConditions() {
+    document.getElementById("tiredness").textContent = jevil.tiredness;
+    document.getElementById("elapsed-turns").textContent = turnsElapsed;
+    const requiredTurns = 30 - Math.floor(jevil.tiredness);
+    document.getElementById("turns-required").textContent = requiredTurns;
+
+    if (jevil.tiredness >= 9 || turnsElapsed >= requiredTurns) {
+        setSparable();
+    }
+}
+
+function setSparable() {
+    const el = document.getElementById("can-spare-text")
+    el.textContent = "CAN";
+    el.className = "sparable";
 }
 
 function onLoad() {
+    document.getElementById("pirouette").addEventListener("click", () => {
+        jevil.tiredness += 0.5;
+        advanceTurn();
+    });
+    document.getElementById("hypnosis").addEventListener("click", () => {
+        jevil.tiredness += 1;
+        advanceTurn();
+    });
+    document.getElementById("turn-none").addEventListener("click", () => {
+        advanceTurn();
+    });
+
     advanceTurn();
 }
 
